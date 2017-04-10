07b4bad70c12866382dd06cc686a6111e4ca9bf8
Matthew Tucker <matt@joinpaladin.com>
2017-04-10 11:35:13 -0400

    Activate vents when navigating to a directory with .venv.

--- zsh/configs/python.zsh
+++ zsh/configs/python.zsh
@@ -1,6 +1,15 @@
-# Initialize pyenv when you nav into a directory
-# eval "$(pyenv init -)"
-# eval "$(pyenv virtualenv-init -)"
+# On cd...
+function chpwd {
+  # Activate virtual env
+  activate_venv
+}
 
-# Use the pyenv virtualenvwrapper plugin
-# eval "pyenv virtualenvwrapper"
+activate_venv() {
+  # If the directory contains a `.venv` subdirectory, activate the venv
+  if [[ -a .venv ]]; then
+    source .venv/bin/activate
+  fi
+}
+
+# Active a venv on new session (if it exists)
+activate_venv
